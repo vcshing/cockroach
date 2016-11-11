03d45bef9b1d5d16418d856c631b2d5db92dffc3
Skyler Liu <vcshing2005@hotmail.com>
2016-11-12 04:34:41 +0800

    ads

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -137,8 +137,8 @@ var app = {
         var weinreUrl;
 		// alert(5);
         document.removeEventListener('deviceready', app.onDeviceReady, false);
-		setTimeout(function() {navigator.splashscreen.hide();}, 1000);
-		navigator.vibrate([1000, 1000, 3000, 1000, 5000, 1000, 5000, 1000, 5000, 1000, 5000, 1000, 5000, 1000, 5000]);
+		setTimeout(function() {navigator.splashscreen.hide();}, 100);
+		//navigator.vibrate([1000, 1000, 3000, 1000, 5000, 1000, 5000, 1000, 5000, 1000, 5000, 1000, 5000, 1000, 5000]);
 		
         if (app.weinre.enabled) {
             console.log('Loading weinre...');
